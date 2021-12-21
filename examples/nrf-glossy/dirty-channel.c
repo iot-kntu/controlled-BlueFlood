372cc3767733959c4a8683175213e31c35a5c841
Salman Maroufi <salman.maroufi@gmail.com>
2021-12-21 12:27:14 +0330

    some changes

--- examples/nrf-glossy/dirty-channel.c
+++ examples/nrf-glossy/dirty-channel.c
@@ -186,10 +186,8 @@ PROCESS_THREAD(tx_process, ev, data)
   int i;
   uint8_t last_rx_ok = 0;
   PROCESS_BEGIN();
-    while(1){
-      PRINTF("%d\n",TEST_HELLO_WORLD);
-    }
-  #if TEST_HELLO_WORLD
+
+  #ifdef TEST_HELLO_WORLD
     my_radio_init(&my_id, my_tx_buffer);
     my_index = get_testbed_index(my_id, testbed_ids, TESTBED_SIZE);
     init_ibeacon_packet(&msg, &uuids_array[0][0], round, slot);
