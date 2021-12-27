05e7246e230ab0164fdab6a313f32b91b0cb0d6a
Salman Maroufi <salman.maroufi@gmail.com>
2021-12-27 12:13:01 +0330

    some changes

--- examples/nrf-glossy/dirty-channel.c
+++ examples/nrf-glossy/dirty-channel.c
@@ -187,6 +187,11 @@ PROCESS_THREAD(tx_process, ev, data)
   int i;
   uint8_t last_rx_ok = 0;
   PROCESS_BEGIN();
+  #if USE_HAMMING_CODE
+    while(1){
+      PRINTF("use hamming\n");
+    }
+  #endif 
   #if TEST_HELLO_WORLD
     my_radio_init(&my_id, my_tx_buffer);
     my_index = get_testbed_index(my_id, testbed_ids, TESTBED_SIZE);
